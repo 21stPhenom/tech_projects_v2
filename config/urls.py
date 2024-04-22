c0b2fa3deab17a2421b5dde58654130a30d4e0ae
21stPhenom <phenom.h.acks21@gmail.com>
2024-04-22 13:15:20 +0100

    update project urls

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('projects.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('projects/', include('projects.urls')),
 ]
